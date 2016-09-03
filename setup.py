e7fa774ec954438a2d5cba7ec82ca2c1cd0f6786
tanyaweaver <tanya83@uw.edu>
2016-09-03 10:59:11 -0700

    added Procfile, run, runapp.py; modified production.ini, setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 
 requires = [
     'pyramid',
+    'pyramid-ipython',
     'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'pyramid_tm',
@@ -23,11 +24,12 @@ tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest',  # includes virtualenv
     'pytest-cov',
+    'tox',
     ]
 
 setup(name='market-analysis',
       version='0.0',
-      description='market-analysis',
+      description='Python app that allows to monitor stock market',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
@@ -35,8 +37,8 @@ setup(name='market-analysis',
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
-      author='',
-      author_email='',
+      author='David Smith, Tatiana Weaver, David Banks',
+      author_email='s.tanya.83@gmail.com',
       url='',
       keywords='web wsgi bfg pylons pyramid',
       packages=find_packages(),
@@ -50,6 +52,6 @@ setup(name='market-analysis',
       [paste.app_factory]
       main = market_analysis:main
       [console_scripts]
-      initialize_market-analysis_db = market_analysis.scripts.initializedb:main
+      init_db = market_analysis.scripts.initializedb:main
       """,
       )
