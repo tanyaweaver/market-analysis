70463702acbddfb123e65d2396c82175ec7693ac
David Smith <dbsmith.dbs83@gmail.com>
2016-09-03 12:55:00 -0700

    updated script to show more about how search works when you dont match one value, and shows content

--- api-experimental/exp1.py
+++ api-experimental/exp1.py
@@ -10,14 +10,22 @@ import requests
 
 
 # example to use the search
-print('Searching for AAPL...')
-resp = requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL')
+print('Searching for AAP...')
+resp = requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAP')
 if resp.status_code == 200:
-    print('Content when searching for apple:')
+    print('Content when searching for "AAP":')
     print(resp.content)
+    print('Grabbing results to show it in a readable form...')
+    for result in resp.json():
+        print('{} {} {}'.format(result['Exchange'], result['Name'], result['Symbol']))
+
 else:
     print('Error connecting to API')
     print(resp.status_code)
+
+print('')
+print('-=' * 30)
+print('')
 
 # example to use Quote
 print('Getting quote info for AAPL')
@@ -25,6 +33,9 @@ resp = requests.get('http://dev.markitondemand.com/Api/v2/Quote/json?symbol=AAPL
 if resp.status_code == 200:
     print('Content when getting a quote for AAPL:')
     print(resp.content)
+    print('Grabbing results to show it in a readable form...')
+    for key, value in resp.json().items():
+        print('{} : {}'.format(key, value))
 else:
     print('Error connecting to API')
     print(resp.status_code)
