19e535d832b275e80ee384f32599d69b831998b4
tanyaweaver <tanya83@uw.edu>
2016-09-03 11:01:27 -0700

    commented out lines 36-45 in initializedb

--- market_analysis/scripts/initializedb.py
+++ market_analysis/scripts/initializedb.py
@@ -33,13 +33,13 @@ def main(argv=sys.argv):
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
 
-    engine = get_engine(settings)
-    Base.metadata.create_all(engine)
+    # engine = get_engine(settings)
+    # Base.metadata.create_all(engine)
 
-    session_factory = get_session_factory(engine)
+    # session_factory = get_session_factory(engine)
 
-    with transaction.manager:
-        dbsession = get_tm_session(session_factory, transaction.manager)
+    # with transaction.manager:
+    #     dbsession = get_tm_session(session_factory, transaction.manager)
 
-        model = MyModel(name='one', value=1)
-        dbsession.add(model)
+    #     model = MyModel(name='one', value=1)
+    #     dbsession.add(model)
