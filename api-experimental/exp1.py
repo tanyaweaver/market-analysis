d6b7e9c5e6f461c7ffa6c4a4f6744688dafb6ace
tanyaweaver <tanya83@uw.edu>
2016-09-03 13:00:13 -0700

    in test

--- api-experimental/exp1.py
+++ api-experimental/exp1.py
@@ -8,23 +8,26 @@ yesterday, and graphs(InteractiveChart).
 
 import requests
 
+def get_response_from_markit():
+    # example to use the search
+    resp = requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL')
+    if resp.status_code == 200:
+        print('Content when searching for apple:')
+        print(resp.content)
+    else:
+        print('Error connecting to API')
+        print(resp.status_code)
 
-# example to use the search
-print('Searching for AAPL...')
-resp = requests.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL')
-if resp.status_code == 200:
-    print('Content when searching for apple:')
-    print(resp.content)
-else:
-    print('Error connecting to API')
-    print(resp.status_code)
+    # example to use Quote
+    print('Getting quote info for AAPL')
+    resp = requests.get('http://dev.markitondemand.com/Api/v2/Quote/json?symbol=AAPL')
+    if resp.status_code == 200:
+        print('Content when getting a quote for AAPL:')
+        print(resp.content)
+    else:
+        print('Error connecting to API')
+        print(resp.status_code)
 
-# example to use Quote
-print('Getting quote info for AAPL')
-resp = requests.get('http://dev.markitondemand.com/Api/v2/Quote/json?symbol=AAPL')
-if resp.status_code == 200:
-    print('Content when getting a quote for AAPL:')
-    print(resp.content)
-else:
-    print('Error connecting to API')
-    print(resp.status_code)
+if __name__ == '__main__':
+    print('Searching for AAPL...')
+    get_response_from_markit()
