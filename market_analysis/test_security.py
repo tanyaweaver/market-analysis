6fcd02a5dc90173a8b30f41188c55bd4b99925d1
crashtack <crashtack@gmail.com>
2016-09-09 12:22:28 -0700

    added some test for admin delete user method

--- market_analysis/test_security.py
+++ market_analysis/test_security.py
@@ -5,20 +5,24 @@ from .conftest import USER_CREDENTIALS
 
 
 def test_public_view(app):
+    '''tests that a public view is public'''
     response = app.get('/public', status='2*')
     assert response.status_code == 200
 
 
 def test_private_view(app):
+    '''tests that security has been implemented'''
     response = app.get('/private', status='4*')
     assert response.status_code == 403
 
 
 def test_user_db_exists(new_session):
+    '''tests that the use database is being setup'''
     assert len(new_session.query(Users).all()) == 0
 
 
 def test_user_gets_added_to_db(new_session):
+    '''tests that a use can be added to the database'''
     user = Users(username=USER_CREDENTIALS['username'], pass_hash='hashiehas')
     new_session.add(user)
     new_session.flush()
@@ -26,6 +30,7 @@ def test_user_gets_added_to_db(new_session):
 
 
 def test_login_view_is_public(app):
+    '''tests that the login view is public'''
     response = app.get('/login', status='2*')
     assert response.status_code == 200
 
@@ -54,36 +59,43 @@ def test_login_bad_user_info(app_and_csrf_token, populated_db):
 
 
 def test_private_view_access_to_authenticated(auth_app, populated_db):
+    '''testing the conftest.py authenticated user loggin fixture works'''
     response = auth_app.get('/private', status='2*')
     assert response.status_code == 200
 
 
 def test_createnewuser_view_is_public(app):
+    '''tests that the creat new user route is public'''
     response = app.get('/new_user', status='2*')
     assert response.status_code == 200
 
 
 def test_logout(app):
+    '''tests that the logout page re-routes the user'''
     response = app.get('/logout', status="3*")
     assert response.status_code == 302
 
 
 def test_home_public(app):
+    '''test that the home view is publicly accessable'''
     response = app.get('/', status="3*")
     assert response.status_code == 302
 
 
 def test_home_rederects_to_portfolio(auth_app, populated_db):
+    '''tests that the "/" route re-routes the user'''
     response = auth_app.get('/', status='3*')
     assert response.status_code == 302
 
 
 def test_admin_is_private(app):
+    '''test that the admin page is not accessable if you are not logged in'''
     response = app.get('/admin', status='4*')
     assert response.status_code == 403
 
 
 def test_admin_accessable_to_adim(admin_app, populated_db_admin):
+    '''testing the admin view is accessable to admin users'''
     app, token = admin_app
     # response = admin_app.get('/admin', status='2*')
     response = app.get('/admin', status='2*')
@@ -91,6 +103,7 @@ def test_admin_accessable_to_adim(admin_app, populated_db_admin):
 
 
 def test_admin_not_accessable_to_non_admin(auth_app, populated_db):
+    '''testing that the admin view is not accessable to non-admin'''
     response = auth_app.get('/admin', status='4*')
     assert response.status_code == 403
 
@@ -106,6 +119,7 @@ def test_admin_delet_post_request_csrf(admin_app, populated_db):
 
 
 def test_new_user_is_public(app):
+    '''checking that the new_user view is public'''
     response = app.get('/new_user', status="2*")
     assert response.status_code == 200
 
